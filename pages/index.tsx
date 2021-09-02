3c4cb3c356263e4787154ea6bbb1017f91f7ec50
BrenoHA <breno.ha@gmail.com>
2021-09-02 00:08:08 -0300

    ✨ Add console with name

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import TimerComponent from '../src/components/Timer';
 import Tilt from 'react-parallax-tilt';
 
 const Home: NextPage = () => {
+  console.log('%cby BrenoHA', 'color: #4f4f; font-family:monospace; font-size: 22px');
+
   return (
     <>
       <Tilt>
